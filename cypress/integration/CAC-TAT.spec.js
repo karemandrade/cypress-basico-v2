0893d72baa5bc21024856c50beac59caca706fe9
Karem Andrade <karemsilva.andrade@gmail.com>
2023-07-31 15:29:11 -0300

    marcando checkbox

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -102,4 +102,28 @@ describe('Central de Atendimento ao cliente TAT', function () {
             .select(1)
             .should('have.value', 'blog')
     })
+
+    it('marca o tipo de atendimento "Feedback"', function () {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+
+    it('marca cada tipo de atendimento', function () {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function($radio){
+                //wrap empacota o elemento e marca
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function () {
+        cy.get('input[type="checkbox"]')
+            .check()
+            .last()
+            .uncheck() 
+            .should('not.be.checked')
+    })
 })
