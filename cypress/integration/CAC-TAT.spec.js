12a273d9ce74b4a9f38eca1228f005aed95b4271
Karem Andrade <karemsilva.andrade@gmail.com>
2023-07-31 15:10:39 -0300

    selecionando elementos

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,6 +15,7 @@ describe('Central de Atendimento ao cliente TAT', function () {
         cy.get('#email').type('karem@teste.com.br')
         cy.get('#open-text-area').type(longText, { delay: 0 })
         cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
@@ -24,7 +25,7 @@ describe('Central de Atendimento ao cliente TAT', function () {
         cy.get('#lastName').type('Andrade')
         cy.get('#email').type('karem@teste,com')
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -41,7 +42,7 @@ describe('Central de Atendimento ao cliente TAT', function () {
         cy.get('#email').type('karem@teste.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -73,14 +74,32 @@ describe('Central de Atendimento ao cliente TAT', function () {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
 
-    it.only('envia o formulário com sucesso usando um comando customizado', function () {
+    it('envia o formulário com sucesso usando um comando customizado', function () {
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (Youtube) por seu texto', function () {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function () {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function () {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,5 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function () {
     cy.get('#lastName').type('Andrade')
     cy.get('#email').type('karem@teste.com')
     cy.get('#open-text-area').type('teste')
-    cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
 })
