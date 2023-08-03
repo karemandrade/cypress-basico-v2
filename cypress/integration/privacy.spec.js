bbdff7e84e4eb08620321fbd589b88877c0ef931
Karem Andrade <karemsilva.andrade@gmail.com>
2023-08-03 16:35:28 -0300

    executando testes várias vezes

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,7 @@
-it('testa a página da política de privacidade de forma independente', function () {
-    cy.visit('./src/privacy.html')
-    cy.contains('Talking About Testing').should('be.visible')
+// teste será executar 3 vezes
+Cypress._.times(3, function(){
+    it('testa a página da política de privacidade de forma independente', function () {
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
